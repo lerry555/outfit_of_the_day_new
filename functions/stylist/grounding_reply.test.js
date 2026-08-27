4f85f6121ae96cd98383d61fcebe91cf2eabb43e
lerry555 <132705754+lerry555@users.noreply.github.com>
2026-08-27 16:55:45 +0200

    test: enforce Brain grounding tykanie fallback

--- functions/stylist/grounding_reply.test.js
+++ functions/stylist/grounding_reply.test.js
@@ -7,6 +7,6 @@ const {groundingClarificationReply} = require("./grounding_reply");
 test("correction grounding never repeats an unsupported location", () => {
   const reply = groundingClarificationReply(["destination", "activity"], true);
   assert.match(reply, /^Máš pravdu, to som si nemal domýšľať\./);
-  assert.match(reply, /Kam sa chystáte/);
-  assert.doesNotMatch(reply, /Martin|turistik|prechádzk/i);
+  assert.match(reply, /Kam sa chystáš/);
+  assert.doesNotMatch(reply, /chystáte|budete|Martin|turistik|prechádzk/i);
 });
